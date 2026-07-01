f5cc5b07d86998d49241629d410658ae0f76837a
yash sharma <ys4468570@gmail.com>
2026-07-01 23:06:17 +0530

    Add nodeman package and initialize project files
    - Created package.json for the project with nodeman as a dependency.
    - Added package-lock.json to lock dependencies.
    - Introduced nodeman's package.json for executable Node.js user's manual.

--- Express js/ex1/app.js
+++ Express js/ex1/app.js
@@ -1,12 +1,45 @@
-const http = require("http");
+// core module
+// const http = require("http");
+// local module
 const userHandler = require("./user");
+// External module
 const express = require("express");
 
-const server = http.createServer((req, res) => {
-  userHandler();
+const app = express();
+app.use("/", (req, res, next) => {
+  console.log("1st middleware", req.url, req.method);
+  res.send(`<h1>Welcome to my hero route</h1>
+    <a href="/submit" ><h2>Click here for submit</h2></a>
+    `);
+  next();
+});
+app.use("/", (req, res, next) => {
+  console.log("1st middleware", req.url, req.method);
+  // res.send(`<h1>Welcome to my hero 2 route</h1>
+  //   <a href="/submit" ><h2>Click here for submit</h2></a>
+  //   `); // could not send request with same route again give an error
+  // but can send middleware
+  next();
+});
+app.use("/submit", (req, res, next) => {
+  console.log("2st middleware", req.url, req.method);
+  res.send("<h1>Logged in successfuly </h1>");
 });
 
-const PORT = "3002";
-server.listen(PORT, () => {
+//before express
+// const server = http.createServer(app);
+
+// const server = http.createServer((req, res) => {
+//   userHandler();
+// });
+
+// const PORT = "3001";
+// server.listen(PORT, () => {
+//   console.log(`server running on http://localhost:${PORT}`);
+// });
+
+// after express
+const PORT = "3001";
+app.listen(PORT, () => {
   console.log(`server running on http://localhost:${PORT}`);
 });
---
+++ Express js/ex1/prac/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("middleware 1", req.url, req.method);
+  next();
+});
+app.use((req, res, next) => {
+  console.log("middleware 2", req.url, req.method);
+  next();
+});
+app.use((req, res, next) => {
+  console.log("middleware 3", req.url, req.method);
+  next();
+});
+app.get("/", (req, res, next) => {
+  console.log("middleware 4", req.url, req.method);
+  res.send("<h1>HOME PAGE </h1>");
+});
+app.get("/contact_us", (req, res, next) => {
+  console.log("middleware 5", req.url, req.method);
+  res.send(`
+    <form action="/contact_us" method="POST">
+      <input type="text" name="name" placeholder="Enter name"><br>
+      <input type="email" name="email" placeholder="Enter email"><br>
+      <button type="submit">Submit</button>
+    </form>
+  `);
+});
+app.post("/contact_us", (req, res) => {
+  console.log("Handling POST request");
+  res.send("<h1>Form Submitted</h1>");
+});
+
+const PORT = 2000;
+app.listen(PORT, () => {
+  console.log(`server is running on http://localhost:${PORT}`);
+});
