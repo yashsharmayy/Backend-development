c79a7f3cdc1e3d9b6d1a08062c9dc40bfb7c4d93
yash sharma <ys4468570@gmail.com>
2026-07-28 16:37:24 +0530

    Add safe-buffer and lodash.once modules for improved buffer handling
    - Introduced safe-buffer module to provide a safer Node.js Buffer API, preventing uninitialized memory issues.
    - Added lodash.once module to enable the use of the lodash `_.once` method as a standalone module.
    - Included necessary files such as LICENSE, README.md, index.js, and package.json for both modules.
    - Updated type definitions for safe-buffer to ensure TypeScript compatibility.

--- Backend2/learn2_Auth_and_Author/back/app.js
+++ Backend2/learn2_Auth_and_Author/back/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 const PORT = 4000;
 app.listen(PORT, () => {
---
+++ Backend2/learn2_Auth_and_Author/back/routes/authRouter.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const authRouter = express.Router();
+
+authRouter.post("/");
