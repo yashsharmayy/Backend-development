8378ab73034a9b868f6134b9bd4a8825dc71aa8b
yash sharma <ys4468570@gmail.com>
2026-07-29 21:28:09 +0530

    Add lodash.once and safe-buffer modules
    - Added lodash.once package with version 4.1.1, including its package.json.
    - Introduced safe-buffer module with its LICENSE, README.md, index.js, index.d.ts, and package.json files.
    - The safe-buffer module provides a safer Node.js Buffer API, addressing security concerns with uninitialized memory.

--- Backend2/spotyfy/backend/controller/userController.js
+++ Backend2/spotyfy/backend/controller/userController.js
@@ -1,8 +1,47 @@
 const userModel = require("../model/user.model");
-
+const jwt = require("jsonwebtoken");
 exports.registerUser = async (req, res) => {
   try {
     const { userName, email, password, role = "user" } = req.body;
-    console.log(req.body);
+
+    const isUserAlreadyExists = await userModel.findOne({
+      $or: [
+        {
+          userName,
+        },
+        {
+          email,
+        },
+      ],
+    });
+    if (isUserAlreadyExists) {
+      return res.status(490).json({
+        message: "user already exists",
+      });
+    }
+
+    const user = await userModel.create({
+      userName,
+      email,
+      password,
+      role,
+    });
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        role: user.role,
+      },
+      process.env.JWT_SECRET,
+    );
+
+    res.cookie("token", token);
+
+    res.status(200).json({
+      message: "user created successfully",
+      user: {
+        id: user._id,
+      },
+    });
   } catch (error) {}
 };
